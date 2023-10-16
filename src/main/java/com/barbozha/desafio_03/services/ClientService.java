3e454242baf7f04f6fa75c477b64537dfc098f32
Barbozha <barbozha@gmail.com>
2023-10-16 17:36:46 -0300

    Busca por cliente inexistente retorna mensagem 404 - quarto commit

--- src/main/java/com/barbozha/desafio_03/services/ClientService.java
+++ src/main/java/com/barbozha/desafio_03/services/ClientService.java
@@ -4,12 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.barbozha.desafio_03.dto.ClientDTO;
 import com.barbozha.desafio_03.entities.Client;
 import com.barbozha.desafio_03.repositories.ClientRepository;
-
-import org.springframework.transaction.annotation.Transactional;
+import com.barbozha.desafio_03.services.exceptions.ResourceNotFoundException;
 
 
 @Service
@@ -18,10 +18,19 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 	
-	
+	//Buscando todos os clientes por página
 	@Transactional(readOnly = true)
 	public Page<ClientDTO> findAll(Pageable pageable) {
 		Page<Client> cli = repository.findAll(pageable);
 		return cli.map(x -> new ClientDTO(x));
 	}
+	
+	//Busca por id retorna cliente existente
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Client cli = repository.findById(id).orElseThrow(
+				() -> new ResourceNotFoundException("cliente inexistente"));
+		return new ClientDTO(cli);
+	}
+	
 }
---
+++ src/main/java/com/barbozha/desafio_03/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.barbozha.desafio_03.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
